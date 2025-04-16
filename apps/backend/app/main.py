2d0aeb5e28d4a8045c7fb9fd837c5b689d2ae184
Kedar Bhide <bhidekedar1@gmail.com>
2025-04-15 21:33:38 -0500

    Main app models fixed

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1 import questions, answers
 from app.database import Base, engine
-from app.models import answer
 from app.api.v1 import auth
 from app.core.scheduler import start_scheduler
 from app.models import answer, user, question
